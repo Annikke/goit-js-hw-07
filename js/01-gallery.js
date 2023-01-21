a1dcaac2ef77b64f248d698ab32b3bcb69afe176
Anna Sova <sova.ania@gmail.com>
2023-01-21 13:03:08 +0100

    task 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,35 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 
-const gallery = document.querySelector('.gallery');
-
+const gallery = document.querySelector(".gallery");
 
 const galleryMarkup = galleryItems
-    .map(item => `<div class="gallery__item">
+  .map(
+    (item) => `<div class="gallery__item">
     <a class="gallery__link" href=${item.original}>
     <img class="gallery__image" src=${item.preview} data-source=${item.original} alt=${item.description}/>
-    </a></div>`)
-    .join('');
-gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+    </a></div>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
 
-gallery.addEventListener('click', (eve) => {
-    eve.preventDefault();
-    if (eve.target.nodeName !== "IMG") {
-        return;
+let instance;
+gallery.addEventListener("click", (eve) => {
+  eve.preventDefault();
+  if (eve.target.nodeName !== "IMG") {
+    return;
+  }
+  instance = basicLightbox.create(
+    `<img src=${eve.target.dataset.source} width='800' height='600'/>`,
+    {
+      onShow: (instance) => {
+        document.addEventListener("keydown", closeOnEsc);
+      },
+      onClose: (instance) => {
+        document.removeEventListener("keydown", closeOnEsc);
+      },
     }
-    const instance = basicLightbox.create(`<img src=${eve.target.dataset.source} width='800' height='600'/>`,
-    { onShow: (instance) => {
-           document.addEventListener('keydown', (eve) => {
-                if (eve.code === "Escape") {
-                    instance.close();
-                    console.log("close");
-                }
-            });
-    }
-    });
-        
-    instance.show();
+  );
+
+  instance.show();
 });
 
+function closeOnEsc(eve) {
+  if (eve.code === "Escape") {
+    instance.close();
+    console.log(eve);
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+  .map(
+    (item) => `<a class="gallery__item" href=${item.original}>
+    <img class="gallery__image" src=${item.preview} alt=${item.description}/>
+    </a></div>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+gallery.addEventListener("click", (eve) => {
+  eve.preventDefault();
+});
+
+const lightbox = new SimpleLightbox(".gallery__item", {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
